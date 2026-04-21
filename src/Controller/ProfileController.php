803fde35d7a57f788149f66eceaced11ab180c08
SrLlama99 <rodrigodemoradelcastillo@gmail.com>
2026-04-21 14:41:59 +0200

    Comenzar a programar el ProfileController y el twig de profile

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ProfileController extends AbstractController
+{
+    #[Route('/profile/{name}', name: 'profile')]
+    public function home(EntityManagerInterface $em, Request $request, $name)
+    {
+        // $this->denyAccessUnlessGranted("ROLE_USER");
+        $rep = $em->getRepository(Usuario::class);
+
+        // Sacar el usuario
+        $usuario = $rep->findOneBy($name);
+
+
+        return $this->render('profile.html.twig', ['user' => $usuario]);
+    }
+}
