61d0ef807359c8764a7ec0787b9db326ca694b43
SrLlama99 <rodrigodemoradelcastillo@gmail.com>
2026-04-24 10:42:21 +0200

    A ver si asi funciona el vendor

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -3,8 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ProfileController extends AbstractController
