3e5c716e4702c716c21c6d840a25c01db3d679e1
SrLlama99 <rodrigodemoradelcastillo@gmail.com>
2026-04-24 10:40:33 +0200

    Principio de controller con orm

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -12,12 +12,23 @@ final class ProfileController extends AbstractController
     #[Route('/profile', name: 'profile')]
     public function home(EntityManagerInterface $em, Request $request)
     {
-        // $this->denyAccessUnlessGranted("ROLE_USER");
-        //$rep = $em->getRepository(Usuario::class);
+        // Repositorios
+        $users = $em->getRepository(Users::class);
+        $carsGarage = $em->getRepository(cocheGaraje::class);
+        $cars = $em->getRepository(Coche::class);
 
         // Sacar el usuario
-        //$usuario = $rep->findOneBy($name);
+        $usuario = $users->findOneBy(['userName' => $name]);
 
+        // Sacar Id del Usuario
+        if($usuario){
+            $idUsuario = $usuario->getId();
+        }
+
+        // Sacar los coches en el garaje del usuario
+        $cochesUsu = $carsGarage->findBy(['usuario' => $idUsuario]);
+
+        // Sacar los coches mejor valorados por el usuario
 
         return $this->render('profile/profile.html.twig');
     }
