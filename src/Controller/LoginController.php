8ba7be85bcacd181076bda47e1ed06bc3f7741b4
Ermenegildo221 <adriannxd10x@gmail.com>
2026-05-13 11:03:00 +0200

    cambiar contraseña terminado

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -34,7 +34,7 @@ final class LoginController extends AbstractController
          $lastUsername = $authenticationUtils->getLastUsername();
 
         // Renderizar el formulario de login
-        return $this->render('login.html.twig', ['last_username' => $lastUsername, 'loginError' => $error, 'registerError' => null, 'checked' => false]);
+        return $this->render('login.html.twig', ['last_username' => $lastUsername, 'loginError' => $error, 'registerError' => null, 'checked' => false, 'checked2' => false]);
     }
 
     #[Route('/crearcuentaProcesa', name:'precesaCuentaNueva')]
@@ -50,11 +50,11 @@ final class LoginController extends AbstractController
         $usuExistente = $eq->findBy(['UserName' => $usuName]);
 
         if($pass1 != $pass2){
-            return $this->render('login.html.twig',["registerError"=>1, "loginError" => null, 'checked' => true]);
+            return $this->render('login.html.twig',["registerError"=>1, "loginError" => null, 'checked' => true, 'checked2' => false]);
         }else if($correoExistente){
-            return $this->render('login.html.twig',["registerError"=>2, "loginError" => null, 'checked' => true]);
+            return $this->render('login.html.twig',["registerError"=>2, "loginError" => null, 'checked' => true, 'checked2' => false]);
         }else if($usuExistente){
-            return $this->render('login.html.twig',["registerError"=>3, "loginError" => null, 'checked' => true]);
+            return $this->render('login.html.twig',["registerError"=>3, "loginError" => null, 'checked' => true, 'checked2' => false]);
         }else{
             $codeVerify = rand(100000,999999);
             $request->getSession()->set("codigoVerificacion", $codeVerify);
@@ -101,19 +101,19 @@ final class LoginController extends AbstractController
             $user->setUserPassword($hashedPassword);
             $entityManager->persist($user);
             $entityManager->flush();
-            return $this->render('login.html.twig', ["registerError"=>5, "loginError" => null, 'checked' => true]);
+            return $this->render('login.html.twig', ["registerError"=>5, "loginError" => null, 'checked' => true, 'checked2' => false]);
         }else{
-             return $this->render('login.html.twig', ["registerError"=>4, "loginError" => null, 'checked' => true]);
+             return $this->render('login.html.twig', ["registerError"=>4, "loginError" => null, 'checked' => true, 'checked2' => false]);
         }
     }
 
     #[Route('/procesaCambiarPass1', name:'procesaPass1')]
     public function procesa1(Request $request, EntityManagerInterface $entityManager, MailerInterface $mailer){
-        $correoRecu = $request->request->get('recoverMail');
+        $correoRecu = $request->request->get('usuMail');
         $request->getSession()->set("correoCambiar", $correoRecu);
 
         $eq = $entityManager->getRepository(Users::class);
-        $correo = $eq->findBy(["UserMail" => $correoRecu]);
+        $correo = $eq->findOneBy(["UserMail" => $correoRecu]);
 
         if($correo){
             $codeVerify = rand(100000,999999);
@@ -130,10 +130,49 @@ final class LoginController extends AbstractController
                 'valiCode' => $codeVerify
                 ]);
             $mailer->send($email);
-            return $this->render('cambiarContraseñas/cambiarPass2.html.twig', ["error" => 0]);
+
+            return $this->render('cambiarPass/cambiarPass1.html.twig', ["registerError" => 0]);
         }else{
-            return $this->render('cambiarContraseñas/cambiarPass.html.twig',["error"=>1]);
+            return $this->render('login.html.twig',["registerError"=>1, "loginError" => null, 'checked2' => true, 'checked' => false]);
         }
     }
 	
+    #[Route('/codeVery', name:'codeVery')]
+    public function veriCode(Request $request){
+        $valiCode = $request->getSession()->get("codeVerify");
+        $recoverValiCode = $request->request->get('recoverValiNumber');
+
+        if($valiCode == $recoverValiCode){
+            return $this->render('cambiarPass/cambiarPass2.html.twig', ["registerError"=>0]);
+        }else{
+            return $this->render('cambiarPass/cambiarPass1.html.twig', ["registerError"=>1]);
+        }
+    }
+
+    #[Route('cambiarPassProcesa3', name:'procesaPass2')]
+    public function cambiarPass(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher){
+        $pass1 = $request->request->get('newPass');
+        $pass2 = $request->request->get('newPass2');
+        $correoSesion = $request->getSession()->get("correoCambiar");
+
+        if($pass1 == $pass2){
+            $eq = $entityManager->getRepository(Users::class);
+            $usuario = $eq->findOneBy(['UserMail'=>$correoSesion]);
+            $plaintextPassword = $pass1;
+
+            $hashedPassword = $passwordHasher->hashPassword(
+                $usuario,
+                $plaintextPassword
+            );
+            if($usuario){
+                $usuario->setUserPassword($hashedPassword);
+                $entityManager->flush();
+            }
+
+            return $this->render('login.html.twig', ["registerError"=>2, "loginError" => null, 'checked' => false, 'checked2' => true]);
+        }else{
+            return $this->render('cambiarPass/cambiarPass2.html.twig', ["registerError"=>1]);
+        }
+    }
+
 }
